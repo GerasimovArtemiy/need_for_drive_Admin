339ac9a1e4cbccf7ab1f539604e0031b0b5d4479
GerasimovArtemiy <artyomgerasim@yandex.ru>
2022-05-11 21:01:45 +0300

    change resposeApi

--- src/API/apiClient.ts
+++ src/API/apiClient.ts
@@ -1,28 +1,26 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import { apiPath } from './apiPath'
 import { BASE_URL, API_KEY, SECRET_KEY } from './keys'
 
 const token = btoa(`10d8c9d:${SECRET_KEY}`)
 
 const responseAPI = axios.create({
     baseURL: BASE_URL,
-    responseType: 'json',
-    headers: {
-        'X-Api-Factory-Application-Id': API_KEY || '',
-        'Access-Control-Allow-Credentials': true,
-        Authorization: `Basic ${token}`,
-        'Content-Type': 'application/json',
-    },
 })
 responseAPI.interceptors.request.use(
     (config: AxiosRequestConfig) => {
-        const bearerToken = localStorage.getItem('accessToken')!
-        if (bearerToken) {
-            config.headers!['Authorization'] = `Bearer ${bearerToken}`
+        config.headers = {
+            'X-Api-Factory-Application-Id': API_KEY || '',
+            Authorization: getAuth(config.url),
         }
         return config
     },
     (error) => Promise.reject(error)
 )
+const getAuth = (path?: string) => {
+    const bearerToken = localStorage.getItem('accessToken')
+    return path === apiPath.login ? `Basic ${token}` : `Bearer ${bearerToken}`
+}
 
 export const apiClient = () => {
     return responseAPI
